e226883833df92610987edd0dd9c2e6b0f9fef71
Joe Tam <someemails@gmail.com>
2022-07-28 00:15:04 -0700

    work at 7-28-22

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// const cbcarousel = document.getElementById('chibi-carousel-photo');
+// const cbthumb1 = document.getElementById('cb_thumb1');
+// const cbthumb2 = document.getElementById('cb_thumb2');
+
+// cbthumb1.addEventListener('click', function() {
+//     cbcarousel.src = "imgs/chibi_sample_resized2.jpeg";
+// });
+
+// cbthumb2.addEventListener('click', function() {
+//     cbcarousel.src = "imgs/chibi_sample_v2.jpeg";
+// });
+// $(function() {
+//     $("#cb_thumb2").click(function() {
+//         $(".carousel").carousel(1);
+//     });
+// });
