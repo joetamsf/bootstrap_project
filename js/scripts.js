842fa191687cbef0c7e3641e348ceb3081e97c08
Joe Tam <someemails@gmail.com>
2022-08-04 00:43:04 -0700

    added aboutme.html and images

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,33 @@
 //         $(".carousel").carousel(1);
 //     });
 // });
+
+const extorigmsg = "Hover over below icons to get information";
+const extwebmsg = document.getElementById('extwebmsg');
+const javadriveimg = document.getElementById('javadriveimg');
+const ebscpaimg = document.getElementById('ebscpaimg');
+const nuimg = document.getElementById('nuimg');
+
+javadriveimg.addEventListener('mouseover', (event) => {
+    extwebmsg.textContent = "Javadrive: A Japanese programming tutorial website for beginners";
+});
+
+javadriveimg.addEventListener('mouseleave', (event) => {
+    extwebmsg.textContent = extorigmsg;
+});
+
+ebscpaimg.addEventListener('mouseover', (event) => {
+    extwebmsg.textContent = "East Bay SCPA: The East Bay's oldest animal welfare organization";
+});
+
+ebscpaimg.addEventListener('mouseleave', (event) => {
+    extwebmsg.textContent = extorigmsg;
+});
+
+nuimg.addEventListener('mouseover', (event) => {
+    extwebmsg.textContent = "NuCamp: Select your bootcamp courses";
+});
+
+nuimg.addEventListener('mouseleave', (event) => {
+    extwebmsg.textContent = extorigmsg;
+});
